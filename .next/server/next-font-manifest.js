07e0f7e5de9ea641da372989ecb34dab76f910b5
AsadullahSamo <asadsamoon8@gmail.com>
2024-04-11 22:46:08 +0500

    add next folder

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{"/components/ChatPage":[],"/components/Connected":[],"/components/Card":[],"/components/Dropdown":[],"/components/Messages":[],"/components/Homepage":[],"/":[]},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
