f976c192862ee03e7f3cde271b581c540bfa23df
AsadullahSamo <asadsamoon8@gmail.com>
2024-04-13 22:15:34 +0500

    add responsiveness to Link preview

--- pages/components/Messages.js
+++ pages/components/Messages.js
@@ -9,7 +9,6 @@ export default function Messages( {messages, nickname, onDeleteMessage, onDelete
     const [dropdowns, setDropdowns] = useState(Array.isArray(messages) ? Array(messages.length).fill(false) : []);
     const [index, setIndex] = useState(0);
     const toggleDropdown = (index) => {
-        console.log(index)
         setDropdowns(prevState => {
             const updatedDropdowns = [...prevState];
             updatedDropdowns[index] = !updatedDropdowns[index];
@@ -32,13 +31,11 @@ export default function Messages( {messages, nickname, onDeleteMessage, onDelete
     } // end of closeDropdown
 
     const handleDeleteClick = (messageIndex) => {
-        console.log(messageIndex)
         onDeleteMessage(messageIndex);
         closeDropdown(messageIndex);
     };
 
     const handleDeleteForMe = (messageIndex) => {
-        console.log(messageIndex)
         onDeleteForMe(messageIndex);
         closeDropdown(messageIndex);
     }
@@ -59,7 +56,7 @@ export default function Messages( {messages, nickname, onDeleteMessage, onDelete
                     <>
                         <span className={`hover:cursor-pointer ${user.name === nickname ? 'self-end right-10' : 'self-start left-10'} top-[4.5rem]  relative text-white text-3xl`} onClick={() => toggleDropdown(messageIndex)}> &#x25be; </span>
                         <div className={`mt-5 rounded-tr-3xl rounded-tl-3xl rounded-br-3xl`}>   
-                            <div className={`${user.name === nickname ? 'flex justify-end mr-10' : 'ml-10'}`}><Card name={user.name} nickname={nickname} messagePart={user.message.substring(0, user.message.indexOf("http"))} siteUrl={user.message.substring(user.message.indexOf("http"))} /></div>                                
+                            <div className={`${user.name === nickname ? 'flex justify-end mr-2 md:mr-10' : 'ml-2 md:ml-10'}`}><Card name={user.name} nickname={nickname} messagePart={user.message.substring(0, user.message.indexOf("http"))} siteUrl={user.message.substring(user.message.indexOf("http"))} /></div>                                
                         </div>
                         {dropdowns[messageIndex] && <div className={`${user.name === nickname ? 'self-end mr-10 -mt-16' : 'ml-96 -mt-16'}`}> <Dropdown name={user.name} nickname={nickname} index={messageIndex} onDeleteClick={() => handleDeleteClick(messageIndex)} onDeleteForMe={() => handleDeleteForMe(messageIndex)}/> </div>}
                         <p className={`${font.poppinsMedium} text-[#737070] -mt-12 ${user.name === nickname ? "self-end mr-10" : 'mx-10'} py-2`}> {user.name === nickname ? "You" : user.name}, <span className='text-[#a2a2a2]'>{user.time}</span> </p>
