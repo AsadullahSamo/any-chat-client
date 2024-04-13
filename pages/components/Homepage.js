70ed03928bb8457e3995e13e6090aa2a2c8efbab
AsadullahSamo <asadsamoon8@gmail.com>
2024-04-13 23:27:10 +0500

    add alt to Image in HomePage component

--- pages/components/Homepage.js
+++ pages/components/Homepage.js
@@ -16,13 +16,13 @@ export default function HomePage() {
     
     <div className='flex md:flex-row flex-col'>
       <div className='w-[100%] h-[10%] md:min-h-screen md:w-[50%] bg-[#edf0f8]'> 
-        <Image src={logo} className={`my-10 mx-10`}/>
+        <Image src={logo} alt="logo" className={`my-10 mx-10`}/>
         <h1 className={`${font.poppinsExtraBold} mt-10 md:mt-56 mx-10 text-xl md:text-5xl leading-snug`}> Chat <br/> anywhere <br/> with anyone </h1>
         <button onKeyDown={handleKeyDown} className='my-10 mx-10 text-white hover:text-black font-semibold hover:border-2 hover:border-solid hover:border-[#434ce6] hover:bg-white hover:cursor-pointer hover:transition-all hover:duration-500 w-36 h-12 rounded-lg bg-[#434CE6]'> <Link href="/components/ChatPage"> Get Started </Link> </button>
       </div>
 
 			<div className='w-[100%] h-[30%] md:min-h-screen md:w-[50%] bg-[#d8dbe3]'> 
-        <Image src={mobilePic} className={`mt-20 mx-auto`} width={450} height={450}/>
+        <Image src={mobilePic} alt="hand holding a phone" className={`mt-20 mx-auto`} width={450} height={450}/>
       </div>
 
     </div>
