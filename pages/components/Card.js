26d61ffee4447ba5c1e14e799dabeb2b0b140c76
AsadullahSamo <asadsamoon8@gmail.com>
2024-04-14 16:29:41 +0500

    remove excessive fetch calls to api and use localStorage caching for better performance and fallback to custom api in case of api limit reached

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -1,82 +1,84 @@
 import React, { useEffect, useState } from 'react';
-import logo from '../../public/assets/icons/logo.svg';
 import font from '../../styles/Fonts.module.css';
 
 export default function Card( { siteUrl, messagePart, name, nickname } ) {
 
-   
+    const [dataFetched, setDataFetched] = useState(true);
     const [loading, setLoading] = useState(true); 
-    const [image, setImage] = useState(logo);
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [favicon, setFavicon] = useState('');
-    const [siteName, setSiteName] = useState('');
-    const [url, setUrl] = useState('');
+    const [dataFromApi, setDataFromApi] = useState({url: '', title: '', description: '', images: [], favicon: '', sitename: ''});
 
+    // Custom api in case of api limit reached
     useEffect(() => {
-        setLoading(true);
-        fetch(`https://any-chat-server.onrender.com/scrape?url=${siteUrl}`)
-        .then(res => res.json())
-        .then(data => {
-          setImage(data.image);
-          setTitle(data.title);
-          setDescription(data.description);
-          setFavicon(data.favicon);
-          setSiteName(data.siteName);
-          setUrl(data.url);
-          setLoading(false);
-        })
-        .catch(error => {
-          console.error('An error occurred:', error);
-          setLoading(false);
-        });
-    }, [siteUrl]);
-
-    // useEffect(() => {
-    //     const apiKey = 'pk_379542c22162196f7598828469e2bc938ccdf938';
-    //     const url = siteUrl;
-    //     const apiUrl = `https://jsonlink.io/api/extract?url=${url}&api_key=${apiKey}`;
-
-    //     fetch(apiUrl)
-    //         .then(response => {
-    //             if (!response.ok) {
-    //                 throw new Error(`Error: ${response.status} - ${response.statusText}`);
-    //             }
-    //             return response.json();
-    //         })
-    //         .then(data => {
-    //             setImage(data.images[0]);
-    //             setTitle(data.title);
-    //             setDescription(data.description);
-    //             setFavicon(data.favicon);
-    //             setSiteName(data.sitename);
-    //             setUrl(data.url);
-    //         })
-    //         .catch(error => {
-    //             console.error('An error occurred:', error);
-    //         });
-    // }, []);
+        if(!dataFetched) {
+            console.log(`I will be executed because api limit is reached`)
+            setLoading(true);
+            fetch(`https://any-chat-server.onrender.com/scrape?url=${siteUrl}`)
+                .then(res => res.json())
+                .then(data => {
+                    setDataFromApi({...data, images: data.image});
+                    setLoading(false);
+            })
+            .catch(error => {
+                console.error('An error occurred:', error);
+                setLoading(false);
+            });
+        }
+    }, []);
+    
+    // Fetch data from localStorage and if data is not in localStorage (new link) fetch from jsonlink api
+    useEffect(() => {
+        let urls = new Map(JSON.parse(localStorage.getItem("urls")))
+        if(urls && urls.has(siteUrl)) {
+            setDataFromApi(urls.get(siteUrl))
+            setLoading(false);
+        } else {
+            const apiKey = 'pk_379542c22162196f7598828469e2bc938ccdf938';
+            const url = siteUrl;
+            const apiUrl = `https://jsonlink.io/api/extract?url=${url}&api_key=${apiKey}`;
+            
+            fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    setDataFetched(false);
+                    setLoading(false);
+                    throw new Error(`Error: ${response.status} - ${response.statusText}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setLoading(false);
+                setDataFromApi(data);
+                const newUrlsMap = urls ? new Map(urls.entries()) : new Map();
+                newUrlsMap.set(siteUrl, data);  
+                localStorage.setItem("urls", JSON.stringify(Array.from(newUrlsMap.entries())));
+            })
+            .catch(error => {
+                console.error('An error occurred:', error);
+                setDataFetched(false);
+            });
+        }
+    }, [dataFetched, siteUrl]);
 
     return (
-        <article className="-mt-5 max-w-sm rounded overflow-hidden shadow-lg" style={{border: '1px solid white'}}>
+        <article className="-mt-5 md:max-w-sm rounded overflow-hidden shadow-lg" style={{border: '1px solid white'}}>
             {messagePart && messagePart.length > 0 && <p className={`bg-[#EDF0F8] break-words text-center ${font.poppinsMedium}`}>{messagePart}</p>  }
             {loading ? (
-                <div>Loading...</div> // Placeholder UI while loading
+                <div>Loading...</div>
             ) : (
             <>
-            <a href={url} target='_blank' rel="noopener noreferrer">
-                <img className="w-full" src={image} alt="image" />
+            <a href={dataFromApi.url} target='_blank' rel="noopener noreferrer">
+                <img className="w-full" src={dataFromApi.images.length > 0 ? dataFromApi.images[0] : ''} alt="image" />
             </a>
             <div className="px-6 py-4">
-                <h2 className={`font-bold text-xl mb-2 ${font.poppinsMedium}`}>{title}</h2>
+                <h2 className={`font-bold text-xl mb-2 ${font.poppinsMedium}`}>{dataFromApi.title}</h2>
                 <p className={`text-gray-700 text-base ${font.poppinsRegular}`}>
-                    {description}
+                    {dataFromApi.description}
                 </p>
             </div>
             <div className="flex gap-2 mx-5 my-2">
-                <img src={favicon} alt="Favicon" style={{ objectFit: "cover" }} />
-                <a href={url} target="_blank" rel="noopener noreferrer" className={`${font.poppinsRegular}`}>
-                    {siteName}
+                <img src={dataFromApi.favicon} alt="Favicon" style={{ objectFit: "cover" }} />
+                <a href={dataFromApi.url} target="_blank" rel="noopener noreferrer" className={`${font.poppinsRegular}`}>
+                    {dataFromApi.sitename}
                 </a>
             </div>
             </>
