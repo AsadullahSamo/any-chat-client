91e0b2e2f85305785090f8f036d77d07ca276e25
AsadullahSamo <asadsamoon8@gmail.com>
2024-05-08 00:03:41 +0500

    fix email regex patters DB storage issue

--- pages/components/Homepage.js
+++ pages/components/Homepage.js
@@ -4,9 +4,11 @@ import logo from '../../public/assets/icons/logo.svg'
 import mobilePic from '../../public/assets/images/hand-holding-a-phone.png'
 import font from '../../styles/Fonts.module.css'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function HomePage() {
 
+  const router = useRouter()
   const [isAccount, setIsAccount] = React.useState(false)
   let [myDetails, setMyDetails] = React.useState('')
 
@@ -21,6 +23,14 @@ export default function HomePage() {
     console.log(myDetails)
   }, [])
 
+  const handleIsAccountRedirect = () => {
+    if (isAccount) {
+      router.push(`/components/Connected?name=${myDetails.name}&email=${myDetails.email}&userID=${myDetails.userID}`)
+    } else {
+      router.push(`/components/ChatPage`)
+    }
+  }
+
   return (
     <>
     <div className='flex md:flex-row flex-col'>
@@ -28,7 +38,7 @@ export default function HomePage() {
       <div className='w-[100%] h-[10%] md:min-h-screen md:w-[50%] bg-[#edf0f8]'>
         <Image src={logo} alt="any chat logo" className={`my-10 mx-10`}/>
         <h1 className={`${font.poppinsExtraBold} mt-10 md:mt-56 mx-10 text-5xl leading-normal`}> Chat <br/> anywhere <br/> with anyone </h1>
-         <Link href={isAccount ? {pathname: `/components/Connected`, query: {name: myDetails.name, email: myDetails.email, userID: myDetails.userID}} : {pathname: "/components/ChatPage"}}> <button className='my-10 mx-10 text-white hover:text-black font-semibold hover:border-2 hover:border-solid hover:border-[#434ce6] hover:bg-white hover:cursor-pointer hover:transition-all hover:duration-500 w-36 h-12 rounded-lg bg-[#434CE6]'> Get Started </button> </Link>
+         <button className='my-10 mx-10 text-white hover:text-black font-semibold hover:border-2 hover:border-solid hover:border-[#434ce6] hover:bg-white hover:cursor-pointer hover:transition-all hover:duration-500 w-36 h-12 rounded-lg bg-[#434CE6]' onClick={handleIsAccountRedirect}> Get Started </button> 
       </div>
 
 			<div className='w-[100%] h-[30%] md:min-h-screen md:w-[50%] bg-[#d8dbe3]'>
