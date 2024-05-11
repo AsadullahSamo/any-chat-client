6b36e8561bba9a9676105a01abb0879b8e1349f4
AsadullahSamo <asadsamoon8@gmail.com>
2024-05-11 14:21:57 +0500

    fix api key setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    JSONLINK_API_KEY: process.env.JSONLINK_API_KEY,
+    EMOJI_API_KEY: process.env.EMOJI_API_KEY,
+  },
 };
 
 export default nextConfig;
