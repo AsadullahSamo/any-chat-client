5edb5898e2bd292df43d5c26078b855518319c09
AsadullahSamo <asadsamoon8@gmail.com>
2024-05-15 00:17:41 +0500

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import HomePage from "@/pages/components/Homepage";
+import Head from "next/head";
 export default function Home() {
 
   return (
+    <>
       <HomePage />
+      <Head>
+          <title>Movie App</title>
+          <link rel="icon" href="/assets/icons/send-message-icon.svg" type="image/icon" />
+      </Head>
+    </>
   );
 
 }
